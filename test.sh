8d1662dc96c0f2677010fff5ff0ec6adb61b3483
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-09-03 13:53:12 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
-#!/usn/bin/env pwsh
-Write-Host "Hello Chavo"
+#!/usr/bin/env bash
+out=$(pwsh hello.ps1)
+
+if [ "$out" == "Hello Chavo" ]; then
+        echo good
+else
+        echo bad
+        exit 1
+fi
